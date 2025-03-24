5d0c95655ea046280a928573ffe493f0c83a183d
刘豪 <liuhao25@foxmail.com>
2025-03-24 11:42:06 +0800

    Update cate.go

--- tiktok/api/product/cate.go
+++ tiktok/api/product/cate.go
@@ -80,6 +80,30 @@ func (b *TiktokProduct) GetCateAttrs(ctx context.Context, token string, cateId s
 	return result
 }
 
+// GetRecommendCate 获取推荐分类
+func (b *TiktokProduct) GetRecommendCate(ctx context.Context, token string, query map[string]string, body map[string]any) RecommendCateResultRsp {
+	//请求接口
+	r := b.SendTiktokApi(ctx, b.GetRecommendCateConfig(ctx, token), query, body, nil)
+	result := RecommendCateResultRsp{
+		Code:     r.Code,
+		Message:  r.Message,
+		HttpCode: r.HttpCode,
+	}
+	if !b.IsSuccess(r) {
+		return result
+	}
+
+	//解析数据
+	err := json.Unmarshal(r.Data, &result)
+	if err != nil {
+		r.Code = common.ErrCode
+		r.Message = "Recommend Category response error " + err.Error()
+		return result
+	}
+
+	return result
+}
+
 // 分类
 func (b *TiktokProduct) GetCateConfig(token string) common.GetApiConfig { //请求方式
 	api := fmt.Sprintf("/product/%s/categories", b.config.Version) //请求API PATH
@@ -113,6 +137,19 @@ func (b *TiktokProduct) GetCateRuleConfig(token string, cateId string) common.Ge
 	return common.GetApiConfig{
 		ContentType: "application/json",         //请求头content-type 类型
 		Method:      "get",                      //请求方法类型
+		Api:         api,                        //请求API PATH地址不带域名
+		FullApi:     b.config.TkApiDomain + api, //请求的API 完整地址，带域名
+		Token:       token,
+	}
+}
+
+// 推荐分类
+func (b *TiktokProduct) GetRecommendCateConfig(ctx context.Context, token string) common.GetApiConfig { //请求方式
+	api := fmt.Sprintf("/product/%s/categories/recommend", b.config.Version) //请求API PATH
+
+	return common.GetApiConfig{
+		ContentType: "application/json",         //请求头content-type 类型
+		Method:      "post",                     //请求方法类型
 		Api:         api,                        //请求API PATH地址不带域名
 		FullApi:     b.config.TkApiDomain + api, //请求的API 完整地址，带域名
 		Token:       token,
